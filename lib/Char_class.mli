822ab7eca02fb064030ee5ae00f28b67bcb6e32c
Joel Burget <joelburget@gmail.com>
2022-01-28 09:09:14 -0800

    Put ring laws in submodule.

--- lib/Char_class.mli
+++ lib/Char_class.mli
@@ -57,28 +57,30 @@ val choose : t -> Uchar.t option
 (** Choose any character from this class. *)
 val choose_exn : t -> Uchar.t
 
-module Ring_laws : sig
-  (** [(a + b) + c = a + (b + c)] *)
-  val plus_associative : t -> t -> t -> bool
+module Laws : sig
+  module Ring : sig
+    (** [(a + b) + c = a + (b + c)] *)
+    val plus_associative : t -> t -> t -> bool
 
-  (** [a + b = b + a] *)
-  val plus_commutative : t -> t -> bool
+    (** [a + b = b + a] *)
+    val plus_commutative : t -> t -> bool
 
-  (** [a + 0 = a] *)
-  val plus_ident : t -> bool
+    (** [a + 0 = a] *)
+    val plus_ident : t -> bool
 
-  (** [a + (-a) = 0] *)
-  val plus_inverse : t -> bool
+    (** [a + (-a) = 0] *)
+    val plus_inverse : t -> bool
 
-  (** [(a * b) * c = a * (b * c)] *)
-  val mul_associative : t -> t -> t -> bool
+    (** [(a * b) * c = a * (b * c)] *)
+    val mul_associative : t -> t -> t -> bool
 
-  (** [a * 1 = a] *)
-  val mul_ident : t -> bool
+    (** [a * 1 = a] *)
+    val mul_ident : t -> bool
 
-  (** [a * (b + c) = (a * b) + (a * c)] *)
-  val left_distributive : t -> t -> t -> bool
+    (** [a * (b + c) = (a * b) + (a * c)] *)
+    val left_distributive : t -> t -> t -> bool
 
-  (** [(b + c) * a = (b * a) + (c * a)] *)
-  val right_distributive : t -> t -> t -> bool
+    (** [(b + c) * a = (b * a) + (c * a)] *)
+    val right_distributive : t -> t -> t -> bool
+  end
 end
