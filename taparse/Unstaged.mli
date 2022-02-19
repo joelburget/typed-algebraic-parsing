46fd2ca017a9820a65ca2b51139535bc215b4618
Joel Burget <joelburget@gmail.com>
2022-02-19 14:23:56 -0800

    Remove redundant signature.

--- taparse/Unstaged.mli
+++ taparse/Unstaged.mli
@@ -1,11 +1,7 @@
-module Make (Token_stream : Signatures.Token_stream) : sig
-  include
-    Signatures.Parser
-      with type token = Token_stream.token
-       and type token_tag = Token_stream.token_tag
-       and type stream = Token_stream.Stream.t
-       and type 'a parser = Token_stream.Stream.t -> 'a
-       and type 'a v = 'a
-
-  val parse : ('ctx, 'a, Type.t) Grammar.t -> 'ctx Parse_env.t -> 'a parser
-end
+module Make (Token_stream : Signatures.Token_stream) :
+  Signatures.Parser
+    with type token = Token_stream.token
+     and type token_tag = Token_stream.token_tag
+     and type stream = Token_stream.Stream.t
+     and type 'a parser = Token_stream.Stream.t -> 'a
+     and type 'a v = 'a
